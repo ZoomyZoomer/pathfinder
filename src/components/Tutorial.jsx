ca0c78159a66e055613701e51fece7faea2cc3fa
ZoomyZoomer <realisticallyspeaking101@gmail.com>
2023-09-17 19:47:19 -0400

    Tutorial Pages

--- src/components/Tutorial.jsx
+++ src/components/Tutorial.jsx
@@ -3,50 +3,73 @@ import video from './videos/demo1.mp4'
 
 
 
+let props = {
+  heading: 'Welcome to PATHFINDER!',
+  subtext: 'PATHFINDER is currently in an early state of development',
+  maintext: "You can get started by clicking on the 'start' and 'end' nodes to move them around",
+  maintext2: "Clicking on empty spaces creates 'blocked' nodes",
+  videolink: video
+};
+
+
+
 
 const Tutorial = () => {
 
-
-
-  let props = {
-    heading: 'Welcome to PATHFINDER!',
-    subtext: 'PATHFINDER is currently in an early state of development',
-    maintext: "You can get started by clicking on the 'start' and 'end' nodes to move them around",
-    maintext2: "Clicking on empty spaces creates 'blocked' nodes"
-  };
+let pageNum = 0;
 
   const [tutorialPage, setTutorialPage] = React.useState(renderPage({...props}));
 
+  function newPage(){
+    switch (pageNum){
+      case 0:
+        pageNum++;
+        props.heading = 'Select an ALGORITHM from the dropbox above';
+        props.subtext = 'More ALGORITHMS are a work-in-progress';
+        props.maintext = '';
+        props.maintext2 = '';
+        return setTutorialPage(renderPage({...props}))
+      case 1:
+        pageNum++;
+        return setTutorialPage(renderPage({...props}))
+    }
+  
+    }
+
+    function clearPage(){
+      setTutorialPage();
+    }
+
   function renderPage(props){
+    console.log(props.heading);
     return(
-      <div>
-        <h1>{props.heading}</h1>
-        <h5 className='fyiMargin'>{props.subtext}</h5>
-        <h3>{props.maintext}</h3>
-        <h3>{props.maintext2}</h3>
-        
-        <button className='tutorial--nextButton'>Next</button>
-      </div>
+      <nav className='tutorial--container'>
+        <div className='tutorial--contents'>
+          <h1>{props.heading}</h1>
+          <h5 className='fyiMargin'>{props.subtext}</h5>
+          <h3>{props.maintext}</h3>
+          <h3>{props.maintext2}</h3>
+          <video 
+            className='demoVideo' src={props.videolink} type="video/mp4" loop autoPlay >
+          </video> 
+          
+          <button className='tutorial--nextButton' onClick={() => newPage()}>Next</button>
+          <button className='tutorial--exitButton' onClick={() => clearPage()}>Exit Tutorial</button>
+        </div>
+      </nav>
     )
     
   }
 
-
-
-
-
-  return (
-        <nav className='tutorial--container'>
-          <div className='tutorial--contents'>
-              {tutorialPage}
-            
-              <video 
-                src={video} type="video/mp4" loop autoPlay >
-              </video>            
-              
-          </div>
-        </nav>
+  
+    return (
+    <div>
+      {tutorialPage}
+    </div>
+       
   )
-}
+    }
+  
+
 
 export default Tutorial
